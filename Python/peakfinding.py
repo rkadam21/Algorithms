6079c170a35f79763b99cbbad687500eb986e3fd
Rahul Kadam <coderk21@gmail.com>
2017-04-11 15:15:44 +0100

    Addition of the peak finding algorithm
    The peak finding algorithm based on MIT lectures from fall 2011

--- Python/peakfinding.py
+++ Python/peakfinding.py
@@ -1,3 +1,31 @@
 import numpy as np
+from math import floor
 
-def findPeak(arr, low, high, n)
+def findPeak(arr):
+    '''Find a peak in 1D-array.
+
+    Function to find a peak (not all peaks) for a 1D-array
+
+    '''
+    mid = floor(len(arr)/2)
+    print("The mid is: ", mid)
+    check = True
+    i = mid
+    l = len(arr)-1
+    while check == True:
+        if (i==0 | i==l):
+            check=False
+        elif arr[i-1] > arr[i]:
+            i=i-1
+        elif arr[i+1] > arr[i]:
+            i=i+1
+        elif ((arr[i] == arr[i+1]) & (arr[i] != arr[i-1])):
+            i=i+1
+        elif ((arr[i] != arr[i+1]) & (arr[i] == arr[i-1])):
+            i=i-1
+        else:
+            check = False
+    print("The index of the peak is: ", i)
+
+if __name__ == '__main__'
+    findPeak()
