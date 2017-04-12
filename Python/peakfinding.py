101d875ef983d4db3798d3dc9e225aeaafaa1230
Rahul Kadam <coderk21@gmail.com>
2017-04-12 13:22:36 +0100

    Update to peakfinding for 2D arrays
    Update to peakfinding for 2D arrays

--- Python/peakfinding.py
+++ Python/peakfinding.py
@@ -8,24 +8,40 @@ def findPeak(arr):
 
     '''
     mid = floor(len(arr)/2)
-    print("The mid is: ", mid)
     check = True
     i = mid
     l = len(arr)-1
     while check == True:
         if (i==0 | i==l):
             check=False
+        elif arr[i-1] <= arr[i] >= arr[i+1]:
+            check = False
         elif arr[i-1] > arr[i]:
             i=i-1
         elif arr[i+1] > arr[i]:
             i=i+1
-        elif ((arr[i] == arr[i+1]) & (arr[i] != arr[i-1])):
-            i=i+1
-        elif ((arr[i] != arr[i+1]) & (arr[i] == arr[i-1])):
-            i=i-1
-        else:
-            check = False
-    print("The index of the peak is: ", i)
+    return i
 
-if __name__ == '__main__'
+def find2DPeak(mat, startj, endj):
+    '''Find a peak for a 2D-array.
+
+    '''
+    jm = floor((endj - startj)/2)
+    igmx = GlobMax([row[jm] for row in mat])
+    if mat[igmx][jm-1] <= mat[igmx][jm] >= mat[igmx][jm+1]:
+        return (igmx, jm)
+    if mat[igmx][jm-1] > mat[igmx][jm]:
+        return find2DPeak([row[startj:jm] for row in mat], startj, jm-1)
+    if mat[igmx][jm+1] > mat[igmx][jm]:
+        return find2DPeak([row[jm+1:] for row in mat], jm+1, endj)
+
+find2DPeak(mat,0,4)
+def GlobMax(arr):
+    imx=0
+    for i in range(len(arr)-1):
+        if arr[i] < arr[i+1]:
+            imx = i+1
+    return imx
+
+if __name__ == '__main__':
     findPeak()
